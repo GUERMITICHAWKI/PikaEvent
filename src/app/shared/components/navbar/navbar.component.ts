48388dc1a7b7d5fe7bc06dc6ef16efd2556a46ea
GUERMITICHAWKI <Guermitichawki@gmail.com>
2026-06-29 10:19:40 +0100

    Navbar + Footer

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, computed, HostListener, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from '../../../core/services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  menuOpen = signal(false);
+  scrolled = signal(false);
 
+  cartCount = computed(() => this.cartService.cartCount());
+
+  constructor(private cartService: CartService) {}
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrolled.set(window.scrollY > 50);
+  }
+
+  toggleMenu() {
+    this.menuOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.menuOpen.set(false);
+  }
 }
